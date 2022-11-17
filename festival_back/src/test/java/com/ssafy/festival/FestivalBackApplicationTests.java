67767177a53dbee2563f70925f6a4220a975576b
서준배 <sjb378@me.com>
2022-11-17 10:51:40 +0900

    init

---
+++ festival_back/src/test/java/com/ssafy/festival/FestivalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.festival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
