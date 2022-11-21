0773ff2dd5b1dfe4cfbbfa6ab2934e88f4123260
서준배 <sjb378@me.com>
2022-11-21 09:53:02 +0900

    modify 게시글 작성 css수정
    최대한 중심에 작은 창으로 구현

--- finalproject_front/src/views/ArticlewriteView.vue
+++ finalproject_front/src/views/ArticlewriteView.vue
@@ -4,34 +4,44 @@
     <h1 class="display-2 text-white">반딧불 축제게시글 작성하기</h1>
   </base-header> 
   
-  <card>
+  <!-- <div class="write-wrap  pt-md-4"> -->
+  <b-container>
     <b-row>
-      말머리
-      <b-form-select v-model="horsehead" :options="horseheadoptions" ></b-form-select>
-    </b-row>
-    <b-row>
-      제목
-      <b-form-input v-model="title" required></b-form-input>
-    </b-row>
-    <b-row>
-      내용
-      <b-form-textarea rows="8" no-resize v-model="content" required></b-form-textarea>
-    </b-row>
-    <b-row>
-      <b-form-file v-model="file2" class="mt-3" plain></b-form-file>
-    </b-row>
-    <b-row align-h="center" align-v="center" >
-      <div>
-        <b-button variant="primary">
-          작성
-        </b-button>
-        <b-button variant="danger">
-          취소
-        </b-button>
-      </div>
+      <b-col class="card-center">
+        <card class="col-md-6">
+          <b-row>
+            말머리
+            <b-form-select v-model="horsehead" :options="horseheadoptions" ></b-form-select>
+          </b-row>
+          <b-row>
+            제목
+            <b-form-input v-model="title" required></b-form-input>
+          </b-row>
+          <b-row>
+            내용
+            <b-form-textarea rows="8" no-resize v-model="content" required></b-form-textarea>
+          </b-row>
+          <b-row>
+            <b-form-file v-model="file" class="mt-3" plain></b-form-file>
+          </b-row>
+          <b-row align-h="center" align-v="center" >
+            <div>
+              <b-button variant="primary">
+                작성
+              </b-button>
+              <b-button variant="danger">
+                취소
+              </b-button>
+            </div>
 
+          </b-row>
+        </card>
+      </b-col>
     </b-row>
-  </card>
+  </b-container>
+
+  
+  <!-- </div> -->
    
 </div>
 </template>
@@ -48,6 +58,7 @@ export default {
     ],
     title:"",
     content:"",
+    file:"",
   }
  }
 }
@@ -55,6 +66,17 @@ export default {
 </script>
 
 <style>
+.write-wrap{
+  display: flex;
+  justify-content: center;
+  width:1000px;
+}
+
+.card-center{
+  display: flex;
+  justify-content: center;
+  /* background-color: red; */
+}
 
 </style>
 
