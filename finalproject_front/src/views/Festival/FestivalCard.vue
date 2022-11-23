bef185c01c014fafeeec27beca7e1073f722449f
JHYUN <jhyun3315@naver.com>
2022-11-24 02:46:44 +0900

    축제 캘린더 월별 조회 완료

--- finalproject_front/src/views/Festival/FestivalCard.vue
+++ finalproject_front/src/views/Festival/FestivalCard.vue
@@ -1,96 +1,33 @@
-<template>
-    <b-card
-        overlay
-        :img-src="checkImage(festivalInfo.originImage)"
-        :img-alt="festivalInfo.festivalName"
-        img-height="455"
-        text-variant="white"
-        class="imgRatio"
-    >
-    <b-card-title>
-        <b-button size="sm" variant="success" @click="goBoard(festivalInfo.festivalId)">게시판으로 </b-button>
-        <b-button size="sm" variant="primary" v-b-modal="festivalInfo.festivalId">상세보기</b-button>
-    </b-card-title>
-    <template #header>
-        <h3 class="mb-0">{{festivalInfo.festivalName}}</h3>
-        <h5  class="mb-0">지역: {{festivalInfo.address}} 일시: {{festivalInfo.startDate}}</h5>
-    </template>
-    <!--상세 축제 정보 모달-->
-    <b-modal  size="lg" :id="festivalInfo.festivalId"  centered :title="festivalInfo.festivalName" button-size="sm" hide-footer>
-        <b-container>
-            <b-row>
-                <h1>{{festivalInfo.festivalName}}</h1>
-            </b-row>
-            <b-row>
-            <b-col>주소 : {{festivalInfo.address}}</b-col>
-            <b-col>날짜 : {{festivalInfo.startDate}}~{{festivalInfo.endDate}}</b-col>
-            </b-row>
-            <b-row>
-            <b-col cols="6">
-                <b-img :src="checkImage(festivalInfo.originImage)" fluid :alt="festivalInfo.festivalName"></b-img>
-            </b-col>
-            <hr class="my-4"/>
-            <b-col cols="6">
-                주관 : {{festivalInfo.orgName}}<br>
-                연락처 : {{festivalInfo.phoneNumber}}<br>
-                <template v-if="festivalInfo.homepage">
-                홈페이지 : <div v-html="festivalInfo.homepage"></div><br>
-                </template>
-                <b-button size="sm" variant="success" @click="goBoard(festivalInfo.festivalId)">게시판으로 </b-button>
-                <b-button size="sm" variant="warning" @click="addFavor(festivalInfo.festivalId)">즐겨찾기 등록</b-button>
-            </b-col>
-            </b-row>
-            <b-row v-html="festivalInfo.festivalContent">
-            </b-row>
-        </b-container>
-    </b-modal>
-    </b-card>
-</template>
-
 <script>
-import {getDefaultImage} from "@/util/boardApi"
-import {favorAdd} from "@/util/favorApi"
-import {mapState, mapGetters, mapActions} from "vuex";
-const memberStore = "memberStore"
+import '@fullcalendar/core/vdom' // solves problem with Vite
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 export default {
-    props:{
-        festivalInfo:Object
-    },
-    methods:{
-        ...mapActions(memberStore, ["getUserInfo"]),
-        checkImage(imgsrc){
-            if(imgsrc){
-                return imgsrc;
-            }else{
-                return getDefaultImage();
-            }
-        },
-        goBoard(festivalId){
-            this.$router.push(`/board/${festivalId}`)
-        },
-        async addFavor(festivalId){
-            //토큰 확인
-            await this.getUserInfo(); // 토큰 확인 및 재발급진행
-            //등록하러가기
-            await favorAdd(
-                festivalId,
-                ({data})=>{
-                    console.log(data)
-                    console.log("등록완료")
-                },
-                ()=>{
-                    console.log("등록실패")
-                    alert("로그인해주세요")
-                }
-                )
-        }
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick,
+        events: [
+          { title: 'event 1', date: '2019-04-01' },
+          { title: 'event 2', date: '2019-04-02' }
+        ]
+      }
     }
+  },
+  methods: {
+    handleDateClick: function(arg) {
+      alert('date click! ' + arg.dateStr)
+    }
+  }
 }
 </script>
-
-<style scoped>
-.imgRatio img{
-    object-fit: contain;
-}
-</style>
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
---
+++ finalproject_front/src/views/tmpFesticalView.vue
@@ -0,0 +1,161 @@
+<template>
+    <div>
+      <base-header class="pb-2 pt-5 pt-md-1 bg-gradient-success">
+        <h1 class="display-2 text-white">지역 축제현황 캘린더</h1>
+      </base-header>
+      <b-container fluid>
+        <b-row>
+          <b-col class="row" id="calframe">
+            <calendar
+              :first-day="1"
+              :all-events="events"
+              @changeDate="changeDate"
+            ></calendar>
+          </b-col>
+          <b-col cols="6">
+            <div
+              id="listgroup-ex"
+              style="overflow-y: auto; width: 600px; height: 850px"
+            >
+              <!--축제 리스트-->
+              <div v-for="(ele, i) in festivals" :key="i">
+                <b-card
+                  no-body
+                  class="overflow-hidden"
+                  style="max-width: 600px; max-height: 400px"
+                >
+                  <b-row no-gutters align-v="center">
+                    <b-col md="6">
+                      <b-card-img
+                        v-if="ele.originImage !== ''"
+                        :src="ele.originImage"
+                        :alt="ele.festivalName"
+                        class="rounded-0"
+                      ></b-card-img>
+                      <b-card-img
+                        v-else
+                        :src="defaultImage()"
+                        alt="기본이미지"
+                        class="rounded-0"
+                      ></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body :title="ele.festivalName">
+                        <b-card-text>
+                          {{ ele.startDate }} ~ {{ ele.endDate }}
+                        </b-card-text>
+                        <b-card-text>
+                          <div
+                            class="festivalContent"
+                            style="overflow-y: auto; width: 270px; height: 200px"
+                          >
+                            {{ ele.festivalContent }}
+                          </div>
+                        </b-card-text>
+                        <div class="buttonRight">
+                          <b-button
+                            size="sm"
+                            variant="primary"
+                            v-b-modal.modal-center
+                            >상세보기</b-button
+                          >
+                          <b-button size="sm" variant="success">
+                            즐겨찾기 등록
+                          </b-button>
+                        </div>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </div>
+              <!-- -->
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+  </template>
+  
+  <script>
+  import { getEveryFestival, getMonthFestival } from "@/util/festivalApi.js";
+  import { getDefaultImage } from "@/util/boardApi";
+  import Calendar from "@/views/Festival/Calendar/Calendar.vue";
+  
+  export default {
+    name: "cal",
+    data() {
+      return {
+        date: "",
+        festivals: [],
+        events: [],
+        test: [],
+        curr: "",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      };
+    },
+    components: {
+      Calendar,
+    },
+    methods: {
+      changeDate(date) {
+        this.date = date;
+        // console.log(date);
+        getMonthFestival(date, ({ data }) => {
+          this.festivals = data.festivalList;
+          console.log(this.festivals);
+        });
+      },
+  
+      defaultImage() {
+        return getDefaultImage();
+      },
+    },
+  
+    mounted() {
+      getEveryFestival(({ data }) => {
+        if (data.status == "ok") {
+          console.log("데이터 테스트");
+          let lst = data.festivalList;
+          this.festivals = data.festivalList;
+          // console.log(this.festivals);
+  
+          for (var i = 0; i < lst.length; i++) {
+            var obj = lst[i];
+            this.events.push({
+              id: obj.festivalId,
+              title: obj.festivalName,
+              color: "panel-danger",
+              date: obj.startDate,
+              end: obj.endDate,
+              info: obj.festivalContent,
+              origin: obj.originImage,
+              thumbnail: obj.thumbnailImage,
+              orgname: obj.orgName,
+              hompage: obj.hompage,
+              phonenumber: obj.phoneNumber,
+            });
+          }
+        } else {
+          alert("Error");
+          this.$router.go();
+        }
+      });
+    }, 
+  };
+  </script>
+  <style>
+  #listgroup-ex {
+    margin-top: 130px;
+    /* margin-left: 10px; */
+  }
+  
+  #calframe {
+    margin-left: 20px;
+    margin-top: 20px;
+  }
+  
+  #listgroup-ex::-webkit-scrollbar {
+    display: none;
+  }
+  </style>
+  
