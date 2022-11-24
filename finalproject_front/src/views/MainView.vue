c8d2280803439098d9c61ceebadd5ce8d80ff389
JHYUN <jiannalee3@gmail.com>
2022-11-24 13:10:39 +0900

    로그인화면 사이드바 하단 수정

--- finalproject_front/src/views/MainView.vue
+++ finalproject_front/src/views/MainView.vue
@@ -1,78 +1,76 @@
 <template>
-  <div> 
-    <base-header class="pb-3 pb-6 pt-5 pt-md-8 bg-gradient-success" > 
- 
-        <div class="wrap-home-resources"> 
-          <div class="home-resources-content">
-            <div class="container">
-              <div class="row">
-                <div class="col-md-12">
-                  <div id="carousel-home-resources" class="carousel-home-resources">
-                    <div v-if="festivals.length">
-                      <carousel-3d :autoplay="true" :autoplay-timeout="5000"  
-                                  :perspective="30"
-                                  :border="0"
-                                  :width="960"
-                                  :height="540"
-                                  :controls-visible="true"
-                                  :space="500"
-                                  :clickable="true">
-                                  
-                        <!-- <slide v-for="(slide, i) in slides" :index="i" :key="i">
+  <div>
+    <base-header class="pb-3 pb-6 pt-5 pt-md-8 bg-gradient-success">
+      <div class="wrap-home-resources">
+        <div class="home-resources-content">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div
+                  id="carousel-home-resources"
+                  class="carousel-home-resources"
+                >
+                  <div v-if="festivals.length">
+                    <carousel-3d
+                      :autoplay="true"
+                      :autoplay-timeout="5000"
+                      :perspective="30"
+                      :border="0"
+                      :width="960"
+                      :height="540"
+                      :controls-visible="true"
+                      :space="500"
+                      :clickable="true"
+                    >
+                      <!-- <slide v-for="(slide, i) in slides" :index="i" :key="i">
                           <img src="https://source.unsplash.com/featured/?festival">
                         </slide> -->
-                        <slide v-for="(ele, i) in festivals" :index="i" :key="i">
-                          <!-- <img :src="checkImage(slide.originImage)"> -->
-                          <festival-card :festivalInfo="ele"></festival-card>
-                        </slide>
-                      </carousel-3d>
-                    </div>
+                      <slide v-for="(ele, i) in festivals" :index="i" :key="i">
+                        <!-- <img :src="checkImage(slide.originImage)"> -->
+                        <festival-card :festivalInfo="ele"></festival-card>
+                      </slide>
+                    </carousel-3d>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>  
-    </base-header>   
-
-     
-
+        </div>
+      </div>
+    </base-header>
   </div>
 </template>
 <script>
-import { Carousel3d, Slide} from 'vue-carousel-3d';
-import {getNowFestival} from "@/util/festivalApi.js"
-import FestivalCard from "@/views/Festival/FestivalBigCard.vue"
+import { Carousel3d, Slide } from "vue-carousel-3d";
+import { getNowFestival } from "@/util/festivalApi.js";
+import FestivalCard from "@/views/Festival/FestivalBigCard.vue";
 
 export default {
-  created(){
+  created() {
     getNowFestival(
-      ({data})=>{
+      ({ data }) => {
         this.festivals = data.festivalList;
       },
-      ()=>{
-        console.log("에러야")
+      () => {
+        console.log("에러야");
       }
-    )
+    );
   },
   components: {
     Carousel3d,
     Slide,
-    FestivalCard
+    FestivalCard,
   },
   data() {
     return {
-      festivals:[]
+      festivals: [],
     };
   },
-  methods: { 
-    
-  }
-}
- 
+  methods: {},
+};
 </script>
-<style>  
-.carousel-home-resources .left-1{ 
+<style>
+.carousel-home-resources .left-1 {
   transform: translateX(-500px) translateZ(-400px) rotateY(-30deg) !important;
 }
 
